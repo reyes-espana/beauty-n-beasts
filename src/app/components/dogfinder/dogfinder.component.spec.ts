7723cda13d69f396b73bdd576813695e53a9d0e6
Courtlyn Boykin <courtlynb@google.com>
2024-08-15 18:09:54 -0400

    fixed broken tests

--- src/app/components/dogfinder/dogfinder.component.spec.ts
+++ src/app/components/dogfinder/dogfinder.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DogfinderComponent } from './dogfinder.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('DogfinderComponent', () => {
   let component: DogfinderComponent;
@@ -8,7 +8,10 @@ describe('DogfinderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DogfinderComponent]
+      imports: [
+        DogfinderComponent,
+        BrowserAnimationsModule
+      ]
     })
     .compileComponents();
     
--- src/app/components/matcherresult/matcherresult.component.spec.ts
+++ src/app/components/matcherresult/matcherresult.component.spec.ts
@@ -1,23 +1,58 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MatcherResultComponent } from './matcherresult.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs'
 
 describe('MatcherResultComponent', () => {
   let component: MatcherResultComponent;
   let fixture: ComponentFixture<MatcherResultComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [MatcherResultComponent]
+  const setupTest = (skinType) => {
+    TestBed.configureTestingModule({
+      imports: [
+        MatcherResultComponent
+      ],
+      providers: [
+        { provide: ActivatedRoute, 
+          useValue: { 
+            snapshot: {
+              params: {
+                selectedSkinType: skinType
+              }
+            }
+          } 
+        },
+      ]
     })
     .compileComponents();
     
     fixture = TestBed.createComponent(MatcherResultComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  };
+
+  it('should create with normal skin type', () => {
+    setupTest('normal');
+     expect(component.selectedSkinType).toBe('normal')
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create with dry skin type', () => {
+    setupTest('dry');
+    expect(component.selectedSkinType).toBe('dry')
+  });
+
+  it('should create with sensitive skin type', () => {
+    setupTest('sensitive');
+     expect(component.selectedSkinType).toBe('sensitive')
+  });
+
+  it('should create with combination skin type', () => {
+    setupTest('combination');
+     expect(component.selectedSkinType).toBe('combination')
+  });
+
+  it('should create with oily skin type', () => {
+    setupTest('oily');
+     expect(component.selectedSkinType).toBe('oily')
   });
 });
